6de6246b29f6ac77decb6b66bc8cc6b82157a191
Nicko Balboa <76141006+NBalboa@users.noreply.github.com>
2024-10-12 12:42:37 +0800

    added dashboard data in hiring manager

--- app/Livewire/Hm/Dashboard.php
+++ app/Livewire/Hm/Dashboard.php
@@ -2,11 +2,44 @@
 
 namespace App\Livewire\Hm;
 
+use App\Enums\JobStatus;
 use App\Enums\Layouts;
+use App\Models\HiringManager;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $total_applicants = 0;
+    public $total_jobs = 0;
+    public $jobs;
+    public $hiring_manager;
+    public $total_pending = 0;
+    public $total_interview = 0;
+    public $total_hired = 0;
+    public $total_rejected = 0;
+    public function mount()
+    {
+        $this->hiring_manager = HiringManager::where('user_id', Auth::user()->id)->first();
+        $this->jobs = $this->hiring_manager->jobs()->get();
+        $this->total_jobs = $this->jobs->count();
+
+        foreach ($this->jobs as $job) {
+            $this->total_applicants += $job->applicants()->count();
+
+
+            $this->total_pending += $this->countStatus($job, JobStatus::PENDING->value);
+            $this->total_interview += $this->countStatus($job, JobStatus::INTERVIEW->value);
+            $this->total_hired += $this->countStatus($job, JobStatus::HIRED->value);
+            $this->total_rejected += $this->countStatus($job, JobStatus::REJECTED->value);
+        }
+    }
+    public function countStatus($job, $status)
+    {
+        $total = $job->applicants()->wherePivot('status', '=', $status)->count();
+
+        return $total;
+    }
     public function render()
     {
         return view('livewire.hm.dashboard')->layout(Layouts::HM->value);
--- resources/views/livewire/hm/dashboard.blade.php
+++ resources/views/livewire/hm/dashboard.blade.php
@@ -1,4 +1,91 @@
 <div>
     {{-- In work, do what you enjoy. --}}
-    dashboard
+    <div class="grid grid-cols-1 gap-4 px-4 mt-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 sm:px-8">
+        <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+            <div class="p-4 bg-pink-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z">
+                    </path>
+                </svg>
+            </div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Jobs</h3>
+                <p class="text-3xl">{{ $total_jobs }}</p>
+            </div>
+        </div>
+        <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+            <div class="p-4 bg-purple-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z">
+                    </path>
+                </svg>
+            </div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Applicants</h3>
+                <p class="text-3xl">{{ $total_applicants }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="grid grid-cols-1 gap-4 px-4 mt-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 sm:px-8">
+        <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+            <div class="p-4 bg-green-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z">
+                    </path>
+                </svg>
+            </div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Pending</h3>
+                <p class="text-3xl">{{ $total_pending }}</p>
+            </div>
+        </div>
+        <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+            <div class="p-4 bg-blue-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z">
+                    </path>
+                </svg>
+            </div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Interview</h3>
+                <p class="text-3xl">{{ $total_interview }}</p>
+            </div>
+        </div>
+        <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+            <div class="p-4 bg-yellow-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z">
+                    </path>
+                </svg>
+            </div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Hired</h3>
+                <p class="text-3xl">{{ $total_hired }}</p>
+            </div>
+        </div>
+        <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+            <div class="p-4 bg-red-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z">
+                    </path>
+                </svg>
+            </div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Rejected</h3>
+                <p class="text-3xl">{{ $total_rejected }}</p>
+            </div>
+        </div>
+    </div>
 </div>
