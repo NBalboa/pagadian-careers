59d20a09f79477936e0d7aa83f4688b1ed0221da
Nicko Balboa <76141006+NBalboa@users.noreply.github.com>
2024-10-12 13:00:40 +0800

    added search hiring_managers in admin

--- app/Livewire/Admin/HiringManager.php
+++ app/Livewire/Admin/HiringManager.php
@@ -7,21 +7,13 @@ use App\Models\HiringManager as ModelsHiringManager;
 use App\Models\User;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
-
-
+use Livewire\WithPagination;
 
 class HiringManager extends Component
 {
-
-    public $hiring_managers;
-
-
-
-    public function mount()
-    {
-        $this->hiring_managers = ModelsHiringManager::with('user', 'company', 'address')->get();
-    }
-
+    use WithPagination;
+    public $search;
+    public $searchBy;
     public function delete($id)
     {
         $hiring_manager = ModelsHiringManager::with('user')->findOrFail($id);
@@ -34,10 +26,49 @@ class HiringManager extends Component
         redirect('hiringmanager')->with(['success' => 'Hiring Manager deleted successfully']);
     }
 
-
+    public function searchJobs()
+    {
+        $this->resetPage();
+    }
     #[Layout('components.admin-layout')]
     public function render()
     {
-        return view('livewire.admin.hiring-manager');
+        $hiring_managers = ModelsHiringManager::with('user', 'company', 'address');
+
+        if (!empty($this->search)) {
+            $search = $this->search;
+            if ($this->searchBy === 'address') {
+                $hiring_managers = $hiring_managers->whereHas('address', function ($query) use ($search) {
+                    $query->whereAny([
+                        'street',
+                        'barangay',
+                        'city',
+                        'province'
+                    ], 'like', '%' . $search . '%');
+                });
+            } else if ($this->searchBy === 'company') {
+                $hiring_managers = $hiring_managers->whereHas('company', function ($query) use ($search) {
+                    $query->whereAny([
+                        'name'
+                    ], 'like', '%' . $search . '%');
+                });
+            } else {
+
+                $hiring_managers = $hiring_managers->whereHas('user', function ($query) use ($search) {
+                    $query->whereAny([
+                        'first_name',
+                        'last_name',
+                        'middle_name',
+                        'email',
+                        'phone_no',
+                        'telephone_no'
+                    ], 'like', '%' . $search . '%');
+                });
+            }
+        }
+        $hiring_managers = $hiring_managers->paginate(10);
+        return view('livewire.admin.hiring-manager', [
+            'hiring_managers' => $hiring_managers
+        ]);
     }
 }
